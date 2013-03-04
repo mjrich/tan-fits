9be94f759a75015135cb7e3d02e5a7806ce7336a
max <maxrichman@gmail.com>
2013-03-04 17:53:40 -0500

    updated layout

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 var TileJSONs = [
-    'http://a.tiles.mapbox.com/v3/examples.map-20v6611k,mapbox.dc-property-values.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-building.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-construction.jsonp',
+    'http://a.tiles.mapbox.com/v3/examples.map-20v6611k,maxrichman.tan-fits-cellaccess.jsonp',
+    'http://a.tiles.mapbox.com/v3/maxrichman.tan-fits-cellaccess.jsonp',
+    'http://a.tiles.mapbox.com/v3/maxrichman.tan-fits-mmuser.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-crime.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-robbery.jsonp',
     'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-theft.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-auto.jsonp'
 ];
 
 $('#map').mapbox(TileJSONs, function(map, tiledata) {
 
     // Assign readable names to all layers
     map.getLayerAt(0).named('base');
-    map.getLayerAt(1).named('building');
-    map.getLayerAt(2).named('construction');
+    map.getLayerAt(1).named('cellaccess');
+    map.getLayerAt(2).named('mmuser');
     map.getLayerAt(3).named('crime');
     map.getLayerAt(4).named('robbery');
     map.getLayerAt(5).named('theft');
-    map.getLayerAt(6).named('auto');
 
     // Don't composite base layer with other layers
     map.getLayer('base').composite(false);
 
     // Disable all overlay layers by default
-    map.disableLayer('building');
-    map.disableLayer('construction');
+    map.disableLayer('cellaccess');
+    map.disableLayer('mmuser');
     map.disableLayer('crime');
     map.disableLayer('robbery');
     map.disableLayer('theft');
-    map.disableLayer('auto');
 
     // Set initial latitude, longitude and zoom level
     map.setCenterZoom({
-        lat: 38.90900,
-        lon: -77.01439
-    }, 13);
+        lat: -6.00953,
+        lon: 37.9805
+    }, 6);
 
     // Set minimum and maximum zoom levels
-    map.setZoomRange(0, 15);
+    map.setZoomRange(4, 12);
 
     // Enable share control
     mapbox.share().map(map).add();
